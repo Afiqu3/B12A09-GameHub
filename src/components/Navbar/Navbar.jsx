a30a7c7382f9509ae328d279f6123cf7526f6ce2
Afiqu3 <gahossainj@gmail.com>
2025-10-22 10:18:52 +0600

    setup project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='text-white'>
+            nav
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+
+const RootLayout = () => {
+    return (
+        <div className='bg-black min-h-screen'>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default RootLayout;
